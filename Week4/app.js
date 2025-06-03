78f6525b199afc06c7c46acaf4dd9144eb7250f5
Ayomide Adedeji <diamonditadedeji@gmail.com>
2025-06-03 16:17:00 +0100

    Intro to Javascript

---
+++ Week4/app.js
@@ -0,0 +1,17 @@
+let a = 5;
+let b = 10;
+
+// This is for addition
+let c = a + b;
+// console.log("The addition of " + a + " and " + b + " is: " + c);
+
+// Using literal template strings
+console.log(`The addition of ${a} and ${b} is: ${c}`);
+
+// console.log("The result of the addition is: " + c);
+
+// This is for subtraction
+let d = b - a;
+
+// console.log(c);
+// console.log(d);
